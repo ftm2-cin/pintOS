49f6e3049c4a327e1dbb9db76957db1bf6881976
agaf2 <agaf2@cin.ufpe.com>
2024-02-13 21:38:29 -0300

    feat: thread_sleep

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -174,6 +174,7 @@ timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
   thread_tick ();
+  thread_wakeup(); // (NOVO) acorda threads bloqueadas
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
