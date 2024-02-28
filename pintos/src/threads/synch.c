913bf2e6e2e7e6d36937d636332c6b1120a8c03d
agaf2 <agaf2@cin.ufpe.com>
2024-02-28 17:51:39 -0300

    Feat: Priority and Preemption

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -68,6 +68,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
+      // (Novo) Ordena a lista de espera de acordo com a prioridade dos threads
       list_push_back (&sema->waiters, &thread_current ()->elem);
       thread_block ();
     }
@@ -295,6 +296,7 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   sema_init (&waiter.semaphore, 0);
+  // (Novo) Ordena a lista de espera de acordo com a prioridade dos threads
   list_push_back (&cond->waiters, &waiter.elem);
   lock_release (lock);
   sema_down (&waiter.semaphore);
